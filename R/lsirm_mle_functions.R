a5fbd6c5abca8bdc2e434f1f268f60dfc573ec1a
Seewoo Li <seewoo1107@naver.com>
2025-06-24 08:13:44 -0700

    parameter recovery 4groups

--- R/lsirm_mle_functions.R
+++ R/lsirm_mle_functions.R
@@ -26,16 +26,20 @@ llik <- function(data, theta, item){
 #' @export
 #'
 #' @examples
-data_generation <- function(seed = 1, N, nitem, gamma = 0.5){
+data_generation <- function(seed = 1, N, nitem=NULL, gamma = 0.5, item=NULL){
   dimension <- 3
   theta <- mvtnorm::rmvnorm(N,
                             mean = rep(0, dimension),
                             sigma = diag(c(1,rep(gamma^2, dimension-1))))
-  item <- cbind(rnorm(nitem),
-                c(rnorm(nitem/2, -gamma/2, gamma/10), rnorm(nitem/2, gamma/2, gamma/10)),
-                0
-                )
-  item[,2:dimension][upper.tri(item[,2:dimension])] <- 0
+  if(is.null(item)){
+    item <- cbind(rnorm(nitem),
+                  c(rnorm(nitem/2, -gamma/2, gamma/10), rnorm(nitem/2, gamma/2, gamma/10)),
+                  0
+                  )
+    item[,2:dimension][upper.tri(item[,2:dimension])] <- 0
+  }else{
+    nitem <- nrow(item)
+  }
 
   data <- matrix(nrow = N, ncol = nitem)
 
@@ -51,6 +55,11 @@ data_generation <- function(seed = 1, N, nitem, gamma = 0.5){
     theta = theta
   ))
 }
+# item <- matrix(c(rep(c(1,-1,0,0), each=5),
+#                  rep(c(0,0,1,-1), each=5)), ncol=2)
+# set.seed(1)
+# item <- cbind(rnorm(10), item)
+# dataset <- data_generation(N = 1000, item = item, gamma = 1)
 
 # E-step
 Estep <- function(data, item, grid, prior = NULL){
@@ -221,20 +230,23 @@ lsirm <- function(data, dimension = 3, range = c(-4,4), q = 11, max_iter = 500, 
     iter <- iter + 1
 
     E <- Estep(data, initial_item, grid, prior)
-    M <- Mstep(E, initial_item, contrast_m, sds)
-    EM_history[[iter]] <- M[[1]]
 
     old_sds <- sds
     factor_means <- as.vector(E$Ak%*%E$grid)
     cov_mat <- t(E$grid) %*% sweep(E$grid, 1, E$Ak, FUN = "*") - factor_means %*% t(factor_means)
     sds <- sqrt(diag(cov_mat))
+    sds[-1] <- sqrt(mean(sds[-1]^2))
+    # sds[-1] <- sds[2]/sds[1]
+    sds[1] <- 1
+
+    M <- Mstep(E, initial_item, contrast_m, sds)
+
     M[[1]] <- sweep(M[[1]], 2, factor_means, FUN = "-")
     M[[1]][which(contrast_m==0)] <- 0
     # M[[1]] <- sweep(M[[1]], 2, old_sds/sds, FUN = "*")
     # M[,1] <- M[,1]/sds[1]
-    sds[-1] <- sqrt(mean(sds[-1]^2))
-    # sds[-1] <- sds[2]/sds[1]
-    sds[1] <- 1
+
+    EM_history[[iter]] <- M[[1]]
 
     ranges <- lapply(sds, function(sd) c(-4, 4) * sd)
     grid_list <- lapply(ranges, function(r) seq(r[1], r[2], length.out = q))
@@ -295,15 +307,12 @@ library(ggplot2)
 #' @import ggplot2
 #'
 #' @examples
-plot.lsirm <- function(item, range = c(-2.5, 2.5)){
-  gamma <- 1
+plot.lsirm <- function(item, range=c(-2.5, 2.5), ls_positions=NULL, gamma=NULL){
+  if(is.null(gamma)) gamma <- 1
   if(is.list(item)){
-    gamma <- sqrt(item$f_cov[2,2])
+    if(is.null(gamma)) gamma <- sqrt(item$f_cov[2,2])
     ls_positions <- as.data.frame(item$theta[,-1])
-    colnames(ls_positions) <- c("coordinate1","coordinate2")
     item <- item$par_est
-  } else{
-    ls_positions <- NULL
   }
 
   if(is.null(rownames(item))){
@@ -322,6 +331,8 @@ plot.lsirm <- function(item, range = c(-2.5, 2.5)){
     ggplot2::labs(title = NULL,
          x = "coordinate 1", y = "coordinate 2")
   if(!is.null(ls_positions)){
+    colnames(ls_positions) <- c("coordinate1","coordinate2")
+
     p <- p +
       ggplot2::geom_point(data = ls_positions,
                  mapping = ggplot2::aes(x = coordinate1, y = coordinate2),
