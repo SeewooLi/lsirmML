51fa085739705379d8731b8e3c5afe61e13280a5
Seewoo Li <seewoo1107@naver.com>
2025-09-05 16:56:17 -0700

    updates for further efficiency: adapted quadrature points and ranges

--- R/lsirm_mle_functions.R
+++ R/lsirm_mle_functions.R
@@ -208,9 +208,10 @@ L1L2_lsirm <- function(e.response, par, grid){
 lsirm <- function(data, dimension = 3, range = c(-4,4), q = 11, max_iter = 500, threshold = 0.001){
   args_list <- as.list(environment())
 
-  x <- seq(range[1], range[2], length.out=q)
-  grid_list <- replicate(dimension, x, simplify = FALSE)
+  ranges <- lapply(c(1,0.6,0.6), function(sd) c(range, q) * sd)
+  grid_list <- lapply(ranges, function(r) seq(r[1], r[2], length.out = r[3]))
   grid <- as.matrix(do.call(expand.grid, grid_list))
+
   prior <- mvtnorm::dmvnorm(grid,
                             mean = rep(0, dimension),
                             sigma = diag(dimension))
@@ -225,7 +226,7 @@ lsirm <- function(data, dimension = 3, range = c(-4,4), q = 11, max_iter = 500, 
   initial_item <- initial_item * contrast_m
 
   iter <- 0
-  EM_history <- list()
+  # EM_history <- list()
   repeat{
     iter <- iter + 1
 
@@ -247,10 +248,10 @@ lsirm <- function(data, dimension = 3, range = c(-4,4), q = 11, max_iter = 500, 
     # M[[1]] <- sweep(M[[1]], 2, old_sds/sds, FUN = "*")
     # M[,1] <- M[,1]/sds[1]
 
-    EM_history[[iter]] <- M[[1]]
+    # EM_history[[iter]] <- M[[1]]
 
-    ranges <- lapply(sds, function(sd) c(-4, 4) * sd)
-    grid_list <- lapply(ranges, function(r) seq(r[1], r[2], length.out = q))
+    ranges <- lapply(c(1,0.6,0.6)*sds, function(sd) c(range, q) * sd)
+    grid_list <- lapply(ranges, function(r) seq(r[1], r[2], length.out = r[3]))
     grid <- as.matrix(do.call(expand.grid, grid_list))
 
     prior <- mvtnorm::dmvnorm(grid,
@@ -276,7 +277,7 @@ lsirm <- function(data, dimension = 3, range = c(-4,4), q = 11, max_iter = 500, 
   return(list(
     par_est = initial_item,
     IM = M[[2]],
-    EM_history = EM_history,
+    # EM_history = EM_history,
     fk=E$freq,
     iter=iter,
     quad=grid,
